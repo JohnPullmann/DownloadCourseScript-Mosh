a0437ac2af55fcf4488a3c6ec03e66f1f878b536
Lubos Noska <lubos.noska12@gmail.com>
2022-11-23 08:52:30 +0100

    Basic program

---
+++ main.py
@@ -0,0 +1,29 @@
+# from bs4 import BeautifulSoup
+# import requests
+
+# def convert_to_html(url) :
+#     result = requests.get(url).text
+#     return BeautifulSoup(result, "html.parser")
+
+# def download_video(name, url):
+#     response = requests.get(url)
+        
+#     with open(name, "wb") as video:
+#             video.write(response.content)
+            
+    
+# doc = convert_to_html("https://codewithmosh.com/courses/python-programming-course-beginners/lectures/6781576")
+
+# group_videos = doc.find_all(class_="col-sm-12 course-section")
+
+# for chapter in group_videos:
+    
+#     idx = 0 
+#     for video in chapter.find_all(class_="section-item incomplete")[:4]:
+#         video = "https://codewithmosh.com/" + video.get("data-lecture-url")
+        
+#         doc = convert_to_html(video)
+#         download_video_btn = doc.find_all(name="a", class_="download")[0].get("href")
+        
+#         idx += 1
+#         download_video(f"video{idx}.mp4", download_video_btn)
